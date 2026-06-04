13d9feafacd7f6b956e292cb486e0303d27d94a3
sonali sonawane <sonawanesonali431@gmail.com>
2026-06-04 13:59:36 +0530

    Added Github User Search Functionality in About.

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,60 @@
 import React from 'react'
-import UserClass from './UserClass';
+import {useState,useEffect} from 'react';
+import search_icon from "../../Assets/search_icon.png";
 
-class About extends React.Component {
-  constructor(props){
-    super(props);
-    console.log("Parent constructor called");
-  }
-  componentDidMount(){
-    console.log("parent component didMount")
-  }
-  render(){
-    console.log("Parent Render called");
-  return (
-    <>
-    <h1>About Us</h1>
+function About() {
+const [user,setUser] = useState(null);
+const[text,setText] = useState("sonalisonawane");
 
-    <UserClass name={"first"} location={"bangalore"} contact={"9970084395"}/>
-    <UserClass name={"second"} location={"bangalore"} contact={"9970084395"}/>
-    <UserClass name={"third"} location={"bangalore"} contact={"9970084395"}/>
+useEffect(() => {
+  const timer = setTimeout(() => {
+    if (text.trim()) {
+      fetchGithubUserInfo();
+    }
+  }, 1000);
+return () => clearTimeout(timer);
+}, [text]);
+
+const fetchGithubUserInfo = async()=>{
+const response = await fetch(`https://api.github.com/users/${text}`);
+const data = await response.json();
+setUser(data);
+}
+
+if (!user) {
+  return <h2>Loading...</h2>;
+}
+const {name,company,location,avatar_url,bio} = user;
+  
+
+return (
+   <>
+   <div className="search">
+    <label htmlFor="input">Search Github Users</label>
+      <input
+        type="text"
+        id="input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        />
+     
+    </div>
+
+    { !user ? (
+      <h2>Loading...</h2>
+      
+    ) :
+    (<div className='user-class'>
+    <div className="profile-img"><img src={avatar_url} alt="" /></div>
+    <div className="profile-info">
+    <p>User Details:{bio}</p><br/>
+    <p>UserName:{name}</p><br/>
+    <p>Company:{company}<br/>       Location:{location}</p>
+    </div>
+    </div>)
+    }
     </>
   )
 }
-}
 
-export default About;
+export default About
